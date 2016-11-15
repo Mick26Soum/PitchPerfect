6c8aaf07fb56972d449c0b233e78ab504206a8c4
Mick Soum <Mick26Soum@gmail.com>
2016-11-14 21:08:33 -0800

    Add AVFoundation/AVAudioRecorder

--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -7,19 +7,66 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+	
+	// MARK: Properties
+	var audioRecorder: AVAudioRecorder!
 
+	@IBOutlet weak var statusLabel: UILabel!
+	
+	@IBOutlet weak var recordButtonLabel: UIButton!
+	
+	@IBOutlet weak var stopRecordButton: UIButton!
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
+		stopRecordButton.isEnabled = false
 	}
 
 	override func didReceiveMemoryWarning() {
 		super.didReceiveMemoryWarning()
 		// Dispose of any resources that can be recreated.
 	}
+	
+	override func viewWillAppear(_ animated: Bool) {
+		print("View Appear Called")
+	}
 
+	@IBAction func recordAudio(_ sender: Any) {
+		statusLabel.text = "Recording In Progress..."
+		recordButtonLabel.isEnabled = false
+		stopRecordButton.isEnabled = true
+		
+		// set the directory path where the recording file will be saved
+		let dirPath = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0] as String
+		let recordingName = "recordedVoice.wav"
+		let pathArray = [dirPath, recordingName]
+		let filePath = URL(string: pathArray.joined(separator: "/"))
+		
+		// create a shared session for AVAudio
+		let session = AVAudioSession.sharedInstance()
+		try! session.setCategory(AVAudioSessionCategoryPlayAndRecord, with:AVAudioSessionCategoryOptions.defaultToSpeaker)
+
+		// Instantiate an audio Recorder
+		// set the properties
+		try! audioRecorder = AVAudioRecorder(url: filePath!, settings: [:])
+		audioRecorder.isMeteringEnabled = true
+		audioRecorder.prepareToRecord()
+		audioRecorder.record()
+		
+	}
+	
+	
+	@IBAction func stopRecording(_ sender: Any) {
+		print("Stop Record Button Pressed")
+		stopRecordButton.isEnabled = false
+		recordButtonLabel.isEnabled = true
+		statusLabel.text = "Recording has stop"
+	}
+	
 
 }
 
